074ec6b97d5d30b40d1ba87ed6d038da50ea60ef
Muhammad Allah Rakha <4444stark@gmail.com>
2021-11-14 15:52:33 +0500

    Update Now

--- HRMS_APP/settings.py
+++ HRMS_APP/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%*gusdhk3azd-^3ugup1dze@4+htojadx_wuzl4d)&(w6wsf5u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1","rakhadon.herokuapp.com"]
 
 
 # Application definition
