9b85847f7bd071092cf8f76a360c4d7bfac154bb
Muhammad Allah Rakha <4444stark@gmail.com>
2021-11-14 16:27:07 +0500

    Ok Done

--- HRMS_APP/settings.py
+++ HRMS_APP/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-%*gusdhk3azd-^3ugup1dze@4+htojadx_wuzl4d)&(w6wsf5u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1","rakhadon.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1","hrmsemployee.herokuapp.com"]
 
 
 # Application definition
